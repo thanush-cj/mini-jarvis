0ddb80516c0b1421e5b67deea099faf47c8994c6
Thanush C J <thanushcj2@gmail.com>
2026-07-03 14:44:28 +0530

    Initial commit: Created Mini Jarvis v1

---
+++ python/Day-05/mini_jarvis.py
@@ -0,0 +1,16 @@
+
+print("===================================")
+print("     Welcome to Mini Jarvis v1")
+print("===================================")
+
+name = input("What is your name? ")
+
+print("Hello,", name + "!")
+print("Nice to meet you.")
+
+dream = input("What is your dream? ")
+
+print("Amazing!")
+print("I hope you achieve your dream of", dream)
+
+print("Mini Jarvis signing off...")
