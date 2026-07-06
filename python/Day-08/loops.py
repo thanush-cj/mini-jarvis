fcc6607b2af5b9b805139ef32ed2e48f3d456aa9
Thanush C J <thanushcj2@gmail.com>
2026-07-06 21:12:42 +0530

    Day 8: Learned loops and upgraded Mini Jarvis V3

---
+++ python/Day-08/loops.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    print("Hello,Engineer Thanush!")
+for i in range(10):
+    print("I will build jarvis")
---
+++ python/Day-08/mini_jarvis_v3.py
@@ -0,0 +1,8 @@
+print("What is your name?")
+name=input()
+count=int(input("How many motivational messages do you want?"))
+print()
+for i in range(count):
+    print(f"{i+1}. Keep going, {name}! Your dream is worth it.")
+print()
+print("Mission accomplised!")
