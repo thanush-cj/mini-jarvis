c19ac02e372c41845ef2447774ac8f8ecc6de090
Thanush C J <thanushcj2@gmail.com>
2026-07-07 21:56:45 +0530

    Day-09 : Learned pyhton functions

--- python/Day-09/functions.py
+++ python/Day-09/functions.py
@@ -5,6 +5,20 @@ def greet():
 def dream():
     print("I will build jarvis")
     print("I will become a great AI engineer")
-def cube(number):
-    print(number*number*number)
-cube(2)
+
+def buissness():
+    print("I will start my own business")
+    print("I will become a successful entrepreneur")
+
+def check_number(num):
+    if num % 2 == 0:
+        print(f"the number {num} is even")
+    else:
+        print(f"the number {num} is odd")
+check_number(8)
+check_number(7)
+
+def rectangular_area(length, width):
+    area = length * width
+    print(f"The area of the rectangle is: {area}")
+rectangualar_area(5,8)
