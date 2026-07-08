1f8856a413c575e1d85712ca35840ffffa28cd7f
Thanush C J <thanushcj2@gmail.com>
2026-07-08 22:06:04 +0530

    Day-10 : Learned return statement

---
+++ python/Day-10/mini_jarvis_v5.py
@@ -0,0 +1,14 @@
+def welcome():
+    return "🤖 Welcome to Mini Jarvis!"
+
+def ask_name():
+    name = input("What is your name? ")
+    return name
+
+message = welcome()
+print(message)
+
+user = ask_name()
+
+print(f"Hello, {user}!")
+print("Have a great day!")
